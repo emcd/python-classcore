51c70ede5364397d394f00fb899c841cd49e0267
Eric McDonald <emcd@users.noreply.github.com>
2025-06-10 19:25:55 -0700

    Start of release 1.5 development.

--- sources/classcore/__init__.py
+++ sources/classcore/__init__.py
@@ -62,7 +62,7 @@ from .factories import *
 
 
 __version__: __.typx.Annotated[ str, __.dynadoc.Visibilities.Reveal ]
-__version__ = '1.4a0'
+__version__ = '1.5a0'
 
 
 standard.dynadoc.assign_module_docstring( __name__, table = __.fragments )
